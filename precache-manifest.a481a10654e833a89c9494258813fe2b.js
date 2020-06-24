47987ea9d2f94a598f7ac8385620a611bea21098
Parshant0171 <parshant0171>
2020-06-24 13:52:37 +0530

    Updates

---
+++ precache-manifest.a481a10654e833a89c9494258813fe2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94238ea4d20551d06db2112bf4e6fd0",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "cdc85264e69d73e30519",
+    "url": "/COVID-19/static/css/main.606d8981.chunk.css"
+  },
+  {
+    "revision": "07598320b096b10dc814",
+    "url": "/COVID-19/static/js/2.652df796.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19/static/js/2.652df796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc85264e69d73e30519",
+    "url": "/COVID-19/static/js/main.e2e5cf3a.chunk.js"
+  },
+  {
+    "revision": "feb19d17d4fd2db4dc10",
+    "url": "/COVID-19/static/js/runtime-main.e84f56aa.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19/static/media/image.d7265326.png"
+  }
+]);
