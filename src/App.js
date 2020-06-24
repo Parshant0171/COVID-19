3f4b4f60ded5f1482dc71a0b1cbe92c7d109bdee
Parshant0171 <parshant0171>
2020-06-24 14:12:46 +0530

    Change of background images and text

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends React.Component {
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} /> 
+        <h3>--- created by Parshant Yadav ---</h3>
       </div>
     );
   }
